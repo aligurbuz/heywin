d71672d394518fcbf4a952518e385f323e7983ad
aliemirgurbuz <ali@haxr.com>
2024-05-20 11:22:00 +0000

    migration for productImage has been created

---
+++ database/migrations/2024_05_20_111859_create_product_images.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_image_code')->default(0)->unique();
+            $table->bigInteger('product_code')->index()->comment('product code');
+
+            $table->text('image')->comment('product image url path');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('product','images','productImage');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+};
