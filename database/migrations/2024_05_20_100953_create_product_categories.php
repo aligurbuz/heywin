aca17707951539c1415f2069bfb828ebd62730d5
aliemirgurbuz <ali@haxr.com>
2024-05-20 10:12:05 +0000

    migration for productCategory has been created

---
+++ database/migrations/2024_05_20_100953_create_product_categories.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_category_code')->default(0)->unique();
+            $table->bigInteger('product_code')->index()->comment('product code');
+            $table->bigInteger('category_code')->index('category_code')->comment('category_code');
+
+            //$table->char('column')->comment('');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('productCategories','store','productCategory');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
