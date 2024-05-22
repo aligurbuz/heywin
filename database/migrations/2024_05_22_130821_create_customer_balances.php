2964b2c804dda089bc1e10751c0c090ccda88633
aliemirgurbuz <ali@haxr.com>
2024-05-22 13:11:21 +0000

    migration for customerBalance has been created

---
+++ database/migrations/2024_05_22_130821_create_customer_balances.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_balances', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_balance_code')->default(0)->unique();
+            $table->bigInteger('customer_code')->index()->comment('customer code');
+
+            $table->bigInteger('payment_type_code')->comment('payment type of the loading balance');
+            $table->double('balance')->comment('the loading balance');
+            $table->bigInteger('currency_code')->comment('currency code of the loading balance');
+
+            //$table->integer('sequence_time')->default(0);
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('balances','customer','customerBalance');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_balances');
+    }
+};
