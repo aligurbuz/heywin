04ae6a7b3128ac9acb13d83eda5aac173498e582
aliemirgurbuz <ali@haxr.com>
2024-06-04 12:31:59 +0000

    migration for customerPayment has been created

---
+++ database/migrations/2024_06_04_123050_create_customer_payments.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_payment_code')->default(0)->unique();
+            $table->bigInteger('currency_code')->comment('currency code');
+
+            $table->double('amount')->comment('price amount');
+
+            $table->json('data')->nullable()->comment('payment response data for provider');
+            //$table->integer('sequence')->default(1);
+
+            $table->boolean('status')->default(1)->comment('0:active 1:passive');
+            $table->boolean('is_deleted')->default(0)->comment('0:notDeleted 1:deleted');
+            $table->bigInteger('created_by')->default(0);
+            $table->bigInteger('updated_by')->default(0);
+            $table->bigInteger('deleted_by')->default(0);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+
+        pushMigration('payments','customer','customerPayment');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_payments');
+    }
+};
